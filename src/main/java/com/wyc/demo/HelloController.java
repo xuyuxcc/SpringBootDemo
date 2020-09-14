6146c96994bd5aae743260fb97204b130f9d4778
wangyongcong <wangyongcong@sunline.cn>
2020-09-14 17:21:29 +0800

    。

---
+++ src/main/java/com/wyc/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.wyc.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("hello")
+
+public class HelloController {
+
+       @RequestMapping("")
+
+       public String hello() {
+
+              return "helloworld2";
+       }
+}
